3709222962624faa490612ba48cc911f3795acbf
Nunder <nathannunderwood@gmail.com>
2022-11-27 20:08:17 -0800

    add header footer imports to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import './App.css';
-import BucketList from './components/BucketList';
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
