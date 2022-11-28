4e44dc26635c549501c8d31f77903612ded7e34b
Nunder <nathannunderwood@gmail.com>
2022-11-27 21:33:12 -0800

    start build header.js

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Navigation from "./Navigation";
+import About from "./About";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+import Resume from "./Resume";
+
+function Header() {
+    const [currentPage, handlePageChange] = useState("About");
+
+const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Contact":
+        return <Contact />;
+      case "Resume":
+        return <Resume />;
+
+      default:
+        return <About />;
+    }
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <a
+            className="navbar-item"
+            rel="noreferrer"
+            target="_blank"
+            href="https://github.com/thundernunder?tab=repositoriess"
+          >
+            <span className="content is-large">Nathan Underwood</span>
+          </a>
+        </div>
+      </nav>
+    
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      
+      <main>
+        <div>{renderPage(currentPage)}</div>
+      </main>
+    </div>
+  );
+}
+
+export default Header;
