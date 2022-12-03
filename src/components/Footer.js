7ff22b1d3c10780ed3f20e9df0df0cc58a1fe460
Nunder <nathannunderwood@gmail.com>
2022-12-03 14:19:42 -0800

    fix footer spacing. still can't figure out images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <strong>My Full Stack Development Portfolio</strong> - a React site by{""}
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
           <a href="https://github.com/thundernunder" target="_blank" rel="noreferrer">
             Nathan Underwood
           </a>
