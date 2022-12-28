5764ebc6bad43b0d0fc521e11854829de1d73ba0
Nunder <nathannunderwood@gmail.com>
2022-12-27 23:13:04 -0800

    border radius avatar image and center align

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ import Background from "../Assets/img/background2.jpg";
 
 function About() {
   return (
-    <div style = {{backgroundImage: `url(${Background})`}}>
+    <div style = {{backgroundImage: `url(${Background})`, borderBottom: "6px solid white"}}>
         <p className="content is-large" style = {{color: "white"}}>Current Marketeer and Aspiring Full Stack Developer</p>
         <hr />
-        <div style = {{display: "flex"}}>
-        <img className="me" src={Nate}alt="Nathan Underwood"/>
+        <div style = {{display: "flex", alignItems: "center"}}>
+        <img className="me" src={Nate}alt="Nathan Underwood" style={{borderRadius: "100%"}}/>
         <div style = {{color: "white", padding: "10px"}}>
           <p id = "aboutP1" className="content is-italic mt-4 content is-large">
           My name is Nathan, but most call me Nate. I currently am an Admissions Director repsonsible for all admissions-marketing efforts at a major private high-school. I am excited to be diving into this journey of becoming a full-stack developer! 
@@ -19,7 +19,9 @@ function About() {
           </p>
         </div>
       </div>
+      <hr />
     </div>
+    
     
   );
 }
